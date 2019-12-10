4e02698c1b4a109bf0dd8bae77c94fa5e634ecea
kenthesurvivor <olivier.inge@gmail.com>
2019-12-10 21:04:49 +0100

    InstallNichoir correction

--- install/installNichoir.sh
+++ install/installNichoir.sh
@@ -724,7 +724,7 @@ IPlocale=$(sudo ifconfig | grep -i -m 1  "netmask 255.255.255.0" | cut -f 10 -d 
 IPexterne=$(dig TXT +short -4 o-o.myaddr.1.google @ns1.google.com | cut -f 2 -d '"')
 Name=$(hostname)
 
-curl --data "ID=$MACaddress&IPEXT=$IPexterne&IPINT=$IPlocale&NAME=$Name" https://ebirds.be/data/identify
+curl --data "ID=$MACaddress&IPEXT=$IPexterne&IPINT=$IPlocale&NAME=$Name&XCOORD=0&YCOORD=0" https://ebirds.be/data/identify
 printError "$?"
 
 #######################################################################
