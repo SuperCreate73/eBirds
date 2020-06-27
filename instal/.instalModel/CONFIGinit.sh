24f3bacdb0387f574374a2c8a8449a1bce3330f6
Olivier Klein <Olivier.Klein@astrid.be>
2020-06-27 20:40:32 +0200

    Instalscript cleanup and corrections

--- instal/.instalModel/CONFIGinit.sh
+++ instal/.instalModel/CONFIGinit.sh
@@ -4,6 +4,9 @@
 #######################################################################
 # installation et configuration des programmes
 #######################################################################
+# TODO Paramètre pour réinitialiser le module fast-cgi dans PHP
+# TODO check parameter cgi.fix_pathinfo in php.ini when php 7.x is loaded
+
 
 # Activation du service PHP fastcgi
 #-----------------------------
@@ -22,69 +25,43 @@ fi
 printMessage "paramétrage" "php-cgi"
 sudo sed /etc/php/7.*/cli/php.ini -i -e "s/^;cgi\.fix_pathinfo=1/cgi\.fix_pathinfo=1/g"
 
+# Paramétrage du fichier /boot/config.txt
+#----------------------------------------
 # activation de la caméra
-#------------------------
-# teste si les lignes de configuration existent déjà
-printMessage "activation de la caméra" "/boot/config.txt"
-if grep -e "^start_x=1$" /boot/config.txt ; then
-	printMessage "caméra déjà activée" "/boot/config.txt"
-else
-	sudo echo -e "\n# activation de la caméra" >> /boot/config.txt
-	sudo echo "start_x=1" >> /boot/config.txt
-	# allocation de mémoire
-	sudo echo "gpu_mem=128" >> /boot/config.txt
-	# désactiver la led de la caméra
-	sudo echo "disable_camera_led=1" >> /boot/config.txt
-fi
+grep -q -e "^start_x=1$" /boot/config.txt || sudo echo -e "\n# activation de la caméra \nstart_x=1" >> /boot/config.txt
+# allocation de mémoire
+grep -q -e "^gpu_mem=128$" /boot/config.txt || sudo echo "gpu_mem=128" >> /boot/config.txt
+# désactiver la led de la caméra
+grep -q -e "^disable_camera_led=1$" /boot/config.txt || sudo echo "disable_camera_led=1" >> /boot/config.txt
 
 # configuration de lighttpd
 #--------------------------
 # teste si la ligne de configuration existe déjà
 printMessage "paramétrage" "lighttpd"
-if sudo grep -e "^	\"mod_fastcgi\",$" /etc/lighttpd/lighttpd.conf ; then
-	printMessage "mod_fastcgi déjà autorisé" "lighttpd"
-else
-	# renomme le fichier de configuration sous lighttpd.conf.bak
-	sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.bak
-	# crée le nouveau fichier de configuration en ajoutant la ligne requise
-# TODO replace by SED command
-	sudo awk '/server.modules/ { print; print "	\"mod_fastcgi\","; next }1' /etc/lighttpd/lighttpd.conf.bak > /etc/lighttpd/lighttpd.conf
-fi
-
+sudo grep -q -e '^ *"mod_fastcgi",$' /etc/lighttpd/lighttpd.conf  || sudo sed -i -e '/^ *server.modules$/a\    "mod_fastcgi",' /etc/lighttpd/lighttpd.conf
+printError "$?"
 
 ########################################################################
 # gestion des groupes et des permissions
 ########################################################################
 printMessage "gestion des permissions des répertoires" ".motion & /var/www"
 
-addgroup w3
-printError "$?"
+addgroup w3 > /dev/null 2>&1 || printError "$?"
 
-adduser pi w3
-printError "$?"
-
-adduser motion w3
-printError "$?"
-
-adduser www-data w3
-printError "$?"
+adduser pi w3 > /dev/null 2>&1 || printError "$?"
+adduser motion w3 > /dev/null 2>&1 || printError "$?"
+adduser www-data w3 > /dev/null 2>&1 || printError "$?"
 
 echo "www-data ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers.d/localsudo
 
-chgrp -R w3 /var/www/
-printError "$?"
+# directory /var/www
+chgrp -R w3 /var/www/ > /dev/null 2>&1 || printError "$?"
+chmod -R 774 /var/www/ > /dev/null 2>&1 || printError "$?"
 
-chmod -R 774 /var/www/
-printError "$?"
-
-mkdir /home/pi/.motion
-printError "$?"
-
-chgrp -R w3 /home/pi/.motion/
-printError "$?"
-
-chmod -R 770 /home/pi/.motion/
-printError "$?"
+# directory /home/pi/.motion
+mkdir /home/pi/.motion > /dev/null 2>&1 || printError "$?"
+chgrp -R w3 /home/pi/.motion/ > /dev/null 2>&1 || printError "$?"
+chmod -R 770 /home/pi/.motion/ > /dev/null 2>&1 || printError "$?"
 
 #######################################################################
 # configuration de l'envoi de l'adresse IP au serveur
@@ -100,21 +77,18 @@ printError "$?"
 #######################################################################
 printMessage "config des actions planifiées" "crontab"
 
-touch /etc/cron.d/ebirdsIP
+echo "0 */6 * * *  root	/var/www/html/public/bash/sendIP " > /etc/cron.d/ebirdsIP
+echo "@reboot      root	/var/www/html/public/bash/sendIP --delay " >> /etc/cron.d/ebirdsIP
 chmod 644 /etc/cron.d/ebirdsIP
 chown root:root /etc/cron.d/ebirdsIP
-echo "0 */6 * * *  root	/var/www/html/public/bash/sendIP " >> /etc/cron.d/ebirdsIP
-echo "@reboot      root	/var/www/html/public/bash/sendIP --delay " >> /etc/cron.d/ebirdsIP
 
-touch /etc/cron.d/ebirdsSensors
+echo "*/30 * * * *  root	/var/www/backend/sensorStart " > /etc/cron.d/ebirdsSensors
+echo "@reboot      root 	/var/www/backend/sensorStart --delay " >> /etc/cron.d/ebirdsSensors
 chmod 644 /etc/cron.d/ebirdsSensors
 chown root:root /etc/cron.d/ebirdsSensors
-echo "*/30 * * * *  root	/var/www/backend/sensorStart " >> /etc/cron.d/ebirdsSensors
-echo "@reboot      root 	/var/www/backend/sensorStart --delay " >> /etc/cron.d/ebirdsSensors
 
-touch /etc/cron.d/ebirdsInOut
+echo "@reboot      root  bash /var/www/backend/ebirds_start --delay " >> /etc/cron.d/ebirdsInOut
 chmod 644 /etc/cron.d/ebirdsInOut
 chown root:root /etc/cron.d/ebirdsInOut
-echo "@reboot      root  bash /var/www/backend/ebirds_start --delay " >> /etc/cron.d/ebirdsInOut
 
 printError "$?"
